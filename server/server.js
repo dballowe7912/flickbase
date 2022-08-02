3df2e7e4bdf08cf9ea771a5ec8045da484a273b3
Donald Ballowe <dballowe7912@gmail.com>
2022-08-02 05:56:32 -0500

    Added user and article models, created mongoDB instance and connected, created nodemon json options file

--- server/server.js
+++ server/server.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 3001
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
 
-app.get('/', (req, res) => {
-    res.send({message: 'hello there'})
-})
+mongoose.connect(process.env.MONGO_URI);
 
+// Parse
+app.use(bodyParser.json);
+
+// Sanitize
+app.use(xss());
+app.use(mongoSanitize());
+
+const PORT = process.env.PORT || 3001;
 app.listen(
     PORT,
-    console.log(`Server is running on port ${PORT}`)
-)
+    () => {
+        console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    }
+);
